a0860b2370ca6281e8c1a617386fdeae53f7816e
Larkin <larkiningram@gmail.com>
2019-11-16 12:21:59 -0600

    players get their correct skills

--- client/src/views/pages/Player.js
+++ client/src/views/pages/Player.js
@@ -7,7 +7,7 @@ import { Input, FormBtn } from "../../components/Form";
 // import { Input, FormBtn, DropDown } from "../../components/Form";
 import API from "../../utils/API"
 
-const emptySkills = []
+// const emptySkills = []
 
 class Player extends Component {
   // Initialize this.state.books as an empty array
@@ -34,12 +34,121 @@ class Player extends Component {
   //function to control the submit button of the search form 
   handleFormSubmit = event => {
     event.preventDefault();
+
+    const allSkills = [
+      "Size",
+      "Accuracy",
+      "Arm Strength",
+      "Release",
+      "Throw on the Run",
+      "Poise",
+      "Pocket Presence",
+      "Decision Making",
+      "Touch",
+      "Instinct",
+      "Footwork",
+      'Elusiveness',
+      'Speed',
+      'Agility',
+      'Vision',
+      'Power',
+      'Tackle Breaking',
+      'Awareness',
+      'Catching',
+      'Acceleration',
+      'Catch',
+      'Catch in Traffic',
+      'Route Running',
+      'Frame',
+      'Pass Block',
+      'Run Block',
+      'Athleticism',
+      'Strength',
+      'Quickness',
+      'Blocking',
+      'Point of Attack',
+      'First Step',
+      'Explosiveness',
+      'Pursuit',
+      'Tackle',
+      'Play Recognition',
+      'Reaction Time',
+      'Coverage',
+      'Ball Skills',
+      'Instincts'
+    ];
+
+    const skillsByPosition = {
+      proStyleQB: [
+        "Size", "Accuracy", "Arm Strength", "Release", "Throw on the Run", "Poise", "Pocket Presence", "Decision Making", "Touch", "Instinct", "Footwork"
+      ]
+      ,
+      dualThreatQB: [
+        "Size", "Accuracy", "Arm Strength", "Release", "Throw on the Run", "Poise", "Pocket Presence", "Decision Making", "Elusiveness"
+      ],
+      runningBack: [
+        "Size", "Speed", "Agility", "Vision", "Elusiveness", "Power", "Tackle Breaking", "Awareness", "Catching"
+      ],
+      wideReceiver: [
+        "Size", "Speed", "Agility", "Acceleration", "Release", "Catch", "Catch in Traffic", "Route Running", "Awareness"
+      ],
+      offensiveTackle: [
+        "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
+      ],
+      tightEnd: [
+        "Size", "Speed", "Release", "Route Running", "Catch", "Catch in Traffic", "Blocking", "Athleticism"
+      ],
+      offensiveGuard: [
+        "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
+      ],
+      center: [
+        "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
+      ],
+      defensiveTackle: [
+        "Size", "Strength", "Point of Attack", "First Step", "Explosiveness", "Athleticism", "Pursuit", "Tackle"
+      ],
+      defensiveEnd: [
+        "Size", "Speed", "Point of Attack", "Frame", "Athleticism", "Strength", "Tackle", "First Step", "Play Recognition", "Pursuit"
+      ],
+      outsideLinebacker: [
+        "Size", "Speed", "Tackle", "Play Recognition", "Athleticism", "Power", "Reaction Time"
+      ],
+      insideLinebacker: [
+        "Size", "Speed", "Tackle", "Play Recognition", "Athleticism", "Power", "Reaction Time"
+      ],
+      cornerback: [
+        "Size", "Speed", "Athleticism", "Coverage", "Ball Skills", "Instincts", "Pursuit", "Tackle"
+      ],
+      safety: [
+        "Size", "Speed", "Athleticism", "Coverage", "Ball Skills", "Instincts", "Pursuit", "Tackle"
+      ],
+      kicker: [
+        "add later"
+      ]
+    }
+
+    function SkillSet (name, points) {
+      this.name = name;
+      this.points = points;
+    }
+    let currentPosition = this.state.position;
+    let skillsNeeded = [];
+    for (var i = 0; i < skillsByPosition[currentPosition].length; i++) {
+      for (var j = 0; j < allSkills.length; j++) {
+        if (skillsByPosition[currentPosition][i] === allSkills[j]) {
+          const po = 5
+          const skill = new SkillSet(allSkills[j], po)
+          skillsNeeded.push(skill)
+        }
+      }
+    }
+
     let newPlayer = {
       firstName: this.state.firstName,
       lastName: this.state.lastName,
       position: {
         name: this.state.position,
-        skills: emptySkills
+        skills: skillsNeeded
       },
       height: this.state.height,
       weight: this.state.weight,
@@ -48,7 +157,7 @@ class Player extends Component {
       film: this.state.film
     }
     console.log(newPlayer)
-    // API.getPlayers()
+
     API.savePlayer(newPlayer)
       .then(res => { this.getPlayers(); console.log(res.data) })
       .catch(err => console.log(err));
@@ -137,12 +246,20 @@ class Player extends Component {
                 name="highschool"
                 placeholder="High School"
               />
-              <Input
-                value={this.state.class}
-                onChange={this.handleInputChange}
-                name="class"
-                placeholder="Class"
-              />
+              <div className="form-group">
+                <select
+                  className="form-control custom-select class"
+                  id="classInput"
+                  name="class"
+                  onChange={this.handleInputChange}
+                >
+                  <option value="N/A">Class</option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                  <option value="2022">2022</option>
+                  <option value="2023">2023</option>
+                </select>
+              </div>
               <Input
                 value={this.state.film}
                 onChange={this.handleInputChange}
--- client/src/views/pages/ProfilePage.js
+++ client/src/views/pages/ProfilePage.js
@@ -59,7 +59,6 @@ class ProfilePage extends Component {
         film: "",
       }
     }, () => {
-      // console.log(this.state)
       this.getPlayerProfile(playerID);
     })
   };
@@ -71,12 +70,10 @@ class ProfilePage extends Component {
   };
 
   getPlayerProfile = (id) => {
-    // this.getRightSkills()
 
     API.getPlayer(id)
       .then(res => {
         let result = res.data;
-        // console.log("skills:", result.position.skills)
 
         let thisPlayer =
         {
@@ -102,115 +99,6 @@ class ProfilePage extends Component {
       .catch(err => console.log(err));;
 
   }
-
-  // getRightSkills = () => {
-  //   const allSkills = {
-  //     "Size": { type: Number, required: false },
-  //     "Accuracy": { type: Number, required: false },
-  //     "Arm Strength": { type: Number, required: false },
-  //     "Release": { type: Number, required: false },
-  //     "Throw on the Run": { type: Number, required: false },
-  //     "Poise": { type: Number, required: false },
-  //     "Pocket Presence": { type: Number, required: false },
-  //     "Decision Making": { type: Number, required: false },
-  //     "Touch": { type: Number, required: false },
-  //     "Instinct": { type: Number, required: false },
-  //     "Footwork": { type: Number, required: false },
-  //     'Elusiveness': { type: Number, required: false },
-  //     'Speed': { type: Number, required: false },
-  //     'Agility': { type: Number, required: false },
-  //     'Vision': { type: Number, required: false },
-  //     'Power': { type: Number, required: false },
-  //     'Tackle Breaking': { type: Number, required: false },
-  //     'Awareness': { type: Number, required: false },
-  //     'Catching': { type: Number, required: false },
-  //     'Acceleration': { type: Number, required: false },
-  //     'Catch': { type: Number, required: false },
-  //     'Catch in Traffic': { type: Number, required: false },
-  //     'Route Running': { type: Number, required: false },
-  //     'Frame': { type: Number, required: false },
-  //     'Pass Block': { type: Number, required: false },
-  //     'Run Block': { type: Number, required: false },
-  //     'Athleticism': { type: Number, required: false },
-  //     'Strength': { type: Number, required: false },
-  //     'Quickness': { type: Number, required: false },
-  //     'Blocking': { type: Number, required: false },
-  //     'Point of Attack': { type: Number, required: false },
-  //     'First Step': { type: Number, required: false },
-  //     'Explosiveness': { type: Number, required: false },
-  //     'Pursuit': { type: Number, required: false },
-  //     'Tackle': { type: Number, required: false },
-  //     'Play Recognition': { type: Number, required: false },
-  //     'Reaction Time': { type: Number, required: false },
-  //     'Coverage': { type: Number, required: false },
-  //     'Ball Skills': { type: Number, required: false },
-  //     'Instincts': { type: Number, required: false },
-  //   };
-
-  //   const skillsByPosition = {
-  //     proStyleQB: [
-  //       "Size", "Accuracy", "Arm Strength", "Release", "Throw on the Run", "Poise", "Pocket Presence", "Decision Making", "Touch", "Instinct", "Footwork"
-  //     ]
-  //     ,
-  //     dualThreatQB: [
-  //       "Size", "Accuracy", "Arm Strength", "Release", "Throw on the Run", "Poise", "Pocket Presence", "Decision Making", "Elusiveness"
-  //     ],
-  //     runningBack: [
-  //       "Size", "Speed", "Agility", "Vision", "Elusiveness", "Power", "Tackle Breaking", "Awareness", "Catching"
-  //     ],
-  //     wideReceiver: [
-  //       "Size", "Speed", "Agility", "Acceleration", "Release", "Catch", "Catch in Traffic", "Route Running", "Awareness"
-  //     ],
-  //     offensiveTackle: [
-  //       "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
-  //     ],
-  //     tightEnd: [
-  //       "Size", "Speed", "Release", "Route Running", "Catch", "Catch in Traffic", "Blocking", "Athleticism"
-  //     ],
-  //     offensiveGuard: [
-  //       "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
-  //     ],
-  //     center: [
-  //       "Size", "Frame", "Pass Block", "Run Block", "Athleticism", "Strength", "Quickness", "Footwork"
-  //     ],
-  //     defensiveTackle: [
-  //       "Size", "Strength", "Point of Attack", "First Step", "Explosiveness", "Athleticism", "Pursuit", "Tackle"
-  //     ],
-  //     defensiveEnd: [
-  //       "Size", "Speed", "Point of Attack", "Frame", "Athleticism", "Strength", "Tackle", "First Step", "Play Recognition", "Pursuit"
-  //     ],
-  //     outsideLinebacker: [
-  //       "Size", "Speed", "Tackle", "Play Recognition", "Athleticism", "Power", "Reaction Time"
-  //     ],
-  //     insideLinebacker: [
-  //       "Size", "Speed", "Tackle", "Play Recognition", "Athleticism", "Power", "Reaction Time"
-  //     ],
-  //     cornerback: [
-  //       "Size", "Speed", "Athleticism", "Coverage", "Ball Skills", "Instincts", "Pursuit", "Tackle"
-  //     ],
-  //     safety: [
-  //       "Size", "Speed", "Athleticism", "Coverage", "Ball Skills", "Instincts", "Pursuit", "Tackle"
-  //     ],
-  //     kicker: [
-  //       "add later"
-  //     ]
-  //   }
-  //   let currentPosition = this.state.player.position;
-  //   // console.log(currentPosition)
-  //   // console.log(skillsByPosition)
-  //   // console.log(allSkills)
-  //   let skillsNeeded = [];
-  //   for (var i = 0; i < skillsByPosition[currentPosition].length; i++) {
-  //     for (var j = 0; j < allSkills.length; j++) {
-  //       if (skillsByPosition[currentPosition][i] === allSkills[j]) {
-  //         skillsNeeded.push(allSkills[j])
-  //       }
-  //     }
-  //   }
-  //   // console.log("skillsNeeded:", skillsNeeded)
-  // }
-
-
 
   render() {
 
