a510d8a0fd86bca8a281ef4712ef2f70b6b1e2c8
Daniel Masters <daniel.p.masters@gmail.com>
2019-12-09 13:24:26 -0600

    finally, a beautiful user authentication flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { useAuth0 } from "./react-auth0-spa";
 import Coach from "./views/pages/Coach";
 import Player from "./views/pages/Player";
 import Home from "./views/pages/Home";
@@ -15,34 +14,30 @@ import "./assets/demo/demo.css";
 // import Login from "./pages/Login";
 // import NoMatch from "./pages/NoMatch";
 import ProfilePage from "./views/pages/ProfilePage";
+import Callback from "./components/Callback"
 import NavBar from "./components/Nav/index";
 
 
 
 
 
-function App() {
-  const { loading } = useAuth0();
-  if (loading) {
-    return (
-      <div>Loading...</div>
-    );
-  } 
+function App(props) {
   return (
     <Router>
       <div>
       
     <div className="App">
       <header>
-        <NavBar />
+        <NavBar {...props}/>
       </header>
     </div>
 
         <Switch>
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
           <Route exact path="/players" component={Player}/>
           <Route exact path="/coaches" component={Coach}/>
           <Route path="/players/:id" component={ProfilePage}/>
+          <Route path="/callback" component={Callback}/>
           {/* <Route path="*" component={NoMatch}/> */}
         </Switch>
       </div>
